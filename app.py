88a83d0c30884e33d5468db0cc1bd70c55fc63e1
YASHMITHA A <yashmithavagman@gmail.com>
2025-09-13 10:47:13 +0530

    updated app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, render_template
-from tensorflow.keras.models import load_model
+from tensorflow import keras
+
 import numpy as np
 import re
 import io
@@ -7,8 +8,10 @@ import base64
 from PIL import Image   
 
 app = Flask(__name__)
-model = load_model('handwritten_digit.keras')
+model = keras.models.load_model('handwritten_digit.keras')
 
 @app.route('/')
 def home():
     return render_template('index.html')
+if __name__ == '__main__':
+    app.run(debug=True)
